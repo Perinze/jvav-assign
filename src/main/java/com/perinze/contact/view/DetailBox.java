32d1699f2ab23c45dde3658113948ecdf9895751
perinze <perinzefaper@foxmail.com>
2022-11-03 19:24:17 +0800

    remove detailbox and rename detaillabel to detailbox

--- src/main/java/com/perinze/contact/view/DetailBox.java
+++ src/main/java/com/perinze/contact/view/DetailBox.java
@@ -4,11 +4,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-public class DetailLabel extends VBox {
+public class DetailBox extends VBox {
     Label label = new Label();
     TextField value = new TextField();
 
-    public DetailLabel(String detail, boolean editable) {
+    public DetailBox(String detail, boolean editable) {
         label.setText(detail);
         value.setEditable(editable);
         value.setText(detail);
