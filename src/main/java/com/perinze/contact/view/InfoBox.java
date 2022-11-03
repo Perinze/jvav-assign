68995082626886970c5e1f7009ce5ca70e594607
perinze <perinzefaper@foxmail.com>
2022-11-03 19:51:03 +0800

    simplify infobox for multi-usages

--- src/main/java/com/perinze/contact/view/InfoBox.java
+++ src/main/java/com/perinze/contact/view/InfoBox.java
@@ -5,15 +5,46 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class InfoBox extends VBox {
-    Label nameLabel = new Label();
-    Label noteLabel = new Label();
+    private DetailBlock name;
+    private DetailBlock phone;
+    private DetailBlock email;
+    private boolean editable;
+    private Contact contact;
 
     public InfoBox() {
-        this.getChildren().addAll(nameLabel, noteLabel);
+        name = new DetailBlock("name");
+        phone = new DetailBlock("phone");
+        email = new DetailBlock("email");
+        editable = false;
+        contact = null;
+
+        this.getChildren().addAll(name, phone, email);
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+        name.setEditable(editable);
+        phone.setEditable(editable);
+        email.setEditable(editable);
+    }
+
+    public Contact get() {
+        if (null == contact) {
+            contact = new Contact();
+        }
+        contact.setName(name.get());
+        contact.setPhone(phone.get());
+        contact.setEmail(email.get());
+        return contact;
     }
 
     public void set(Contact contact) {
-        nameLabel.setText(contact.getName());
-        noteLabel.setText(contact.getNote());
+        this.contact = contact;
+        if (null != contact) {
+            name.set(contact.getName());
+            phone.set(contact.getPhone());
+            email.set(contact.getEmail());
+        }
     }
+
 }
