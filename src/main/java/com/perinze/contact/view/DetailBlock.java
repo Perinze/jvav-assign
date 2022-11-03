7baa1546af0826fbfcb9bdc77c124812fba0781b
perinze <perinzefaper@foxmail.com>
2022-11-03 19:27:34 +0800

    rename

--- src/main/java/com/perinze/contact/view/DetailBlock.java
+++ src/main/java/com/perinze/contact/view/DetailBlock.java
@@ -4,11 +4,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
-public class DetailBox extends VBox {
+public class DetailBlock extends VBox {
     Label label = new Label();
     TextField value = new TextField();
 
-    public DetailBox(String detail, boolean editable) {
+    public DetailBlock(String detail, boolean editable) {
         label.setText(detail);
         value.setEditable(editable);
         value.setText(detail);
